fdea0d6473a5509a07526b88a372822de8fbeb6d
Mateusz Matusiak <mateusz.matusiak@ardentcode.com>
2020-11-11 17:26:49 +0100

    Updates

---
+++ precache-manifest.c2196cf019cc3daebb9639a000068ac2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa73b6915791cc5a8dab390fdb6fb06",
+    "url": "/styl-karkonoski/index.html"
+  },
+  {
+    "revision": "17d526ed3e92d08234bb",
+    "url": "/styl-karkonoski/static/css/main.2437fb13.chunk.css"
+  },
+  {
+    "revision": "c3a3b65f84892fc6b97e",
+    "url": "/styl-karkonoski/static/js/2.2f048457.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/styl-karkonoski/static/js/2.2f048457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d526ed3e92d08234bb",
+    "url": "/styl-karkonoski/static/js/main.26821ca0.chunk.js"
+  },
+  {
+    "revision": "3767f7971b75e14cfa74",
+    "url": "/styl-karkonoski/static/js/runtime-main.a5efbeee.js"
+  },
+  {
+    "revision": "427105a26a2bd90c9398bef5670460d2",
+    "url": "/styl-karkonoski/static/media/karkonosze.427105a2.jpg"
+  },
+  {
+    "revision": "e3e859f1172848ad84dca8d92a0a97e4",
+    "url": "/styl-karkonoski/static/media/tshirt1.e3e859f1.png"
+  },
+  {
+    "revision": "b0424b922264d52e76940beb375f59e7",
+    "url": "/styl-karkonoski/static/media/tshirt2.b0424b92.png"
+  },
+  {
+    "revision": "14f1c5b42eb081df994ab576ed0a2157",
+    "url": "/styl-karkonoski/static/media/tshirt3.14f1c5b4.png"
+  }
+]);
